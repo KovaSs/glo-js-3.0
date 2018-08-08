015b6e7346b67be3e971a900b62e1e907dd37ff5
Denis Isaev <sr_relikt@mail.ru>
2018-08-08 19:07:48 +0300

    Lesson 4 - hard task

--- les4/hard/js/script.js
+++ les4/hard/js/script.js
@@ -8,14 +8,35 @@ let	output = [],
 
 for (let i = 0, len = sNumber.length; i < len; i++) {
     output.push(+sNumber.charAt(i));
+
 }
 
 //Сложение 
 for (var i = 0, sum = 0; i < output.length; sum += output[i++]);
-//Умножение
-for (var i = 0, mult = 0; i < output.length; mult *= output[i++]);
 
+//Умножение
+for ( var i = 0, mult = 1; i < output.length; mult *= output[i++]);
+
+// Полученный результат возвести в степень 3, используя только 1 оператор
+let mathPow = Math.pow(mult, 3);
+
+// alert(`Результат возведения в 3 степень числа ${mult} равен ${mathPow}`);
+
+//Вывести его на экран первые 2 цифры полученного числа
+
+let twoNum = [],
+    twoNumber = mathPow.toString();
+
+for ( var i = 0; i < 2; i++) {
+	twoNum.push(twoNumber.charAt(i));
+};
+for ( var i = 0, two = ''; i < 2; two += twoNum[i++]);
+
+
+console.log(typeof mathPow);
 
 console.log(output);
 console.log(`Сумма цифр числа ${num} равна ${sum}`);
 console.log(`Произведение цифр числа ${num} равна ${mult}`);
+console.log(`Результат возведения в 3 степень числа ${mult} равен ${mathPow}`);
+console.log(`Первые 2 цифры числа ${mathPow} равны ${two}`);
